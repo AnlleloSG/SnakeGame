3854d40689a68edf70ba47f926b516fbb2577f55
AnlleloSG <david.almarazcstll@uanl.edu.mx>
2021-07-26 16:56:36 -0500

    Se agrego opcion de modo de juego y el juego termina cuando acaben los objetos

--- Snake.py
+++ Snake.py
@@ -8,17 +8,22 @@ POS_Y = 1
 MAP_WIDTH = 20
 MAP_HEIGHT = 15
 NUM_OF_NUMBER_OBJECTS = 10
+game_mode = 0
+
+while game_mode != 1 and game_mode != 2:
+	game_mode = int(input("Choose Game mode:\n[1] Phantom Walls\n[2] Border Walls\nA:"))
 
 tail_length = 1
 tail = []
 my_position = [random.randint(1,MAP_WIDTH-1),random.randint(1,MAP_HEIGHT-1)]
 map_objects = []
+
 while len(map_objects) < NUM_OF_NUMBER_OBJECTS:
 	new_position = [random.randint(1,MAP_WIDTH-1),random.randint(1,MAP_HEIGHT-1)]
 	if new_position not in map_objects and new_position != my_position:
 		map_objects.append(new_position)
 
-while True:
+while len(map_objects) > 0:
 	os.system("cls")
 	print("+" + "-" * MAP_WIDTH * 3 + "+" )
 	for cordinate_y in range(MAP_HEIGHT):
@@ -52,35 +57,33 @@ while True:
 	print("A donde te quieres mover? [WASD] [Any other letter to quit]")
 	direction = readchar.readchar().decode()
 
-	if direction.upper() == "W":
-		my_position[POS_Y] -= 1
-		my_position[POS_Y] %= MAP_HEIGHT
-	elif direction.upper() == "S":
-		my_position[POS_Y] += 1
-		my_position[POS_Y] %= MAP_HEIGHT
-	elif direction.upper() == "A":
-		my_position[POS_X] -= 1
-		my_position[POS_X] %= MAP_WIDTH
-	elif direction.upper() == "D":
-		my_position[POS_X] += 1
-		my_position[POS_X] %= MAP_WIDTH
+	if game_mode == 1:
+		if direction.upper() == "W":
+			my_position[POS_Y] -= 1
+			my_position[POS_Y] %= MAP_HEIGHT
+		elif direction.upper() == "S":
+			my_position[POS_Y] += 1
+			my_position[POS_Y] %= MAP_HEIGHT
+		elif direction.upper() == "A":
+			my_position[POS_X] -= 1
+			my_position[POS_X] %= MAP_WIDTH
+		elif direction.upper() == "D":
+			my_position[POS_X] += 1
+			my_position[POS_X] %= MAP_WIDTH
+		else:
+			exit()
 	else:
-		exit()
-
+		if direction.upper() == "W" and ((my_position[POS_Y] - 1) > -1):
+			my_position[POS_Y] -= 1
+		elif direction.upper() == "S" and ((my_position[POS_Y] + 1) < MAP_HEIGHT):
+			my_position[POS_Y] += 1
+		elif direction.upper() == "A" and ((my_position[POS_X] - 1) > -1):
+			my_position[POS_X] -= 1
+		elif direction.upper() == "D" and ((my_position[POS_X] + 1) < MAP_WIDTH):
+			my_position[POS_X] += 1
+		else:
+			print("Can't touch walls. YOU DIED.")
+			exit(0)
+	
 	tail.insert(0,my_position.copy())
 	tail = tail[:tail_length]
-	
-	#If you want sanke to stops in the walls then use this
-	"""
-	if direction.upper() == "W" and ((my_position[POS_Y] - 1) > -1):
-		my_position[POS_Y] -= 1
-	elif direction.upper() == "S" and ((my_position[POS_Y] + 1) < MAP_HEIGHT):
-		my_position[POS_Y] += 1
-	elif direction.upper() == "A" and ((my_position[POS_X] - 1) > -1):
-		my_position[POS_X] -= 1
-	elif direction.upper() == "D" and ((my_position[POS_X] + 1) < MAP_WIDTH):
-		my_position[POS_X] += 1
-	elif direction.upper() == "Q":
-		exit(0)
-	"""
-	#Just Line added
